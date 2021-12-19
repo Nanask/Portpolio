ca7b0ef9ee56e7b9f680821e6cab92147d301f0d
Nanask <ybhag1005@daum.net>
2021-12-19 23:00:58 +0900

    mode

---
+++ src/provider/ModeProvider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ModeProvider() {
+  return <div></div>;
+}
+
+export default ModeProvider;
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+const light = {
+  backgroundColor: "while",
+};
+
+const dark = {
+  backgroundColor: "#fff",
+};
+
+export const light = { light };
+export const light = { dark };
