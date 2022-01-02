01f55d093145e124be4f9b7e35187a95081f487d
Nanask <ybhag1005@daum.net>
2022-01-02 23:01:17 +0900

    포폴

--- src/provider/ModeProvider.jsx
+++ src/provider/ModeProvider.jsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useContext, useState } from "react";
 import useDarkMode from "use-dark-mode";
-import { ThemeProvier } from "styled-components";
 import { createContext } from "react";
 
 const ThemeContext = createContext();
@@ -9,34 +8,30 @@ export const useThemecontext = () => {
   return useContext(ThemeContext);
 };
 
-function ModeProvider({ Children }) {
+const ModeProvider = ({ Children }) => {
   const [theme, setTheme] = useState("light");
+
+  function useTheme() {
+    const context = useContext(ThemeContext);
+    const { theme, setTheme } = context;
+
+    const toggleTheme = useCallback(() => {
+      if (theme === "light") {
+        setTheme("dark");
+      } else {
+        setTheme("light");
+      }
+    }, [theme]);
+
+    return [theme, setTheme];
+  }
 
   const propsStore = {
     theme,
     setTheme,
   };
 
-  return (
-    <ThemeContext.Provider value={propsStore}>
-      <ThemeProvier theme={themeObject}>{Children}</ThemeProvier>
-    </ThemeContext.Provider>
-  );
-}
+  return <ThemeContext.Provider value={propsStore}>{Children}</ThemeContext.Provider>;
+};
 
-function useTheme() {
-  const context = useContext(ThemeContext);
-  const { theme, setTheme } = context;
-
-  const toggleTheme = useCallback(() => {
-    if (Theme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  }, [theme]);
-
-  return [theme, setTheme];
-}
-
-export default ModeProvider({ ThemeProvier, useTheme });
+export default ModeProvider;
